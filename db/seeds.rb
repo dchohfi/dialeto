b403f07629a1d39a747e6b669796108b8ceca7c8
Diego Chohfi <dchohfi@gmail.com>
2011-06-01 02:26:11 -0300

    Atualizado seed
    Signed-off-by: Diego Chohfi <dchohfi@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 perfil = Perfil.find_or_create_by_descricao("Administrador")
-usuario = User.find_by_email('dchohfi@gmail.com')
+usuario = User.find_by_email('demo@demo.com')
 if(!usuario)
-  usuario = User.new(email:'dchohfi@gmail.com', password:'123456', password_confirmation:'123456', remember_me:false, perfil:perfil, role:'admin')
+  usuario = User.new(email:'demo@demo.com', password:'123456', password_confirmation:'123456', remember_me:false, perfil:perfil, role:'admin')
   usuario.save!
 end
