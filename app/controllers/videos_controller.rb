c2ff6da4d941baad842aea5d075802e78461a8d0
Diego Chohfi <dchohfi@gmail.com>
2011-08-03 20:02:37 -0300

    Corrigido action show do video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -64,7 +64,7 @@ class VideosController < ApplicationController
     end
     
     @original_video = @video.panda_video
-    @h264_encoding = @original_video.encodings.find_by_profile_name("h264")
+    @h264_encoding = @original_video.encodings.first
 
     if @video
       respond_to do |format|
