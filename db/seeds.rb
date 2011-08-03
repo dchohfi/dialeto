7b71fffa50a57f2a4478c1f32ba50d02fca30ab4
Diego Chohfi <dchohfi@gmail.com>
2011-08-03 19:54:43 -0300

    Corrigido seed para cadastro completo de usuario

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 perfil = Perfil.find_or_create_by_descricao("Administrador")
 usuario = User.find_by_email('demo@demo.com')
 if(!usuario)
-  usuario = User.new(:email => 'demo@demo.com', :password => '123456', :password_confirmation => '123456', :remember_me => false, :perfil => perfil, :role => 'admin')
+  usuario = User.new(:email => 'demo@demo.com', :password => '123456', :password_confirmation => '123456', :remember_me => false, :perfil => perfil, :role => 'admin', :cpf => '381.819.658-33', :telefone => '12-88144904', :nome => 'Diego Chohfi', :nascimento => Date.strptime("08/12/1987", '%d/%m/%Y'), :sexo => 'masculino', :username => 'demo')
   usuario.save!
 end
